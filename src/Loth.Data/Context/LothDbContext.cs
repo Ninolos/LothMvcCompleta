6a163f4c7cc4a9f60d9bdacae36f71cb8156d108
Jonathan Daniel Loth <77141599+Ninolos@users.noreply.github.com>
2023-01-11 10:10:06 -0300

    #dev - init mapping entidades para o Context

---
+++ src/Loth.Data/Context/LothDbContext.cs
@@ -0,0 +1,21 @@
+﻿using AppLothMVC.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loth.Data.Context
+{
+    public class LothDbContext : DbContext
+    {
+        public LothDbContext(DbContextOptions options) : base(options)
+        {
+               
+        }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+    }
+}
---
+++ src/Loth.Data/Mappings/ProdutoMapping.cs
@@ -0,0 +1,19 @@
+﻿using AppLothMVC.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loth.Data.Mappings
+{
+    public class ProdutoMapping : IEntityTypeConfiguration<Produto>
+    {
+        public void Configure(EntityTypeBuilder<Produto> builder)
+        {
+            builder.HasKey(x => x.Id);
+        }
+    }
+}

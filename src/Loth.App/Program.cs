24a1e5299b3348e9a82662203b7108983b90e685
Jonathan Daniel Loth <77141599+Ninolos@users.noreply.github.com>
2023-01-12 09:56:32 -0300

    #dev - add migrations and update database

--- src/Loth.App/Program.cs
+++ src/Loth.App/Program.cs
@@ -1,4 +1,5 @@
 using Loth.App.Data;
+using Loth.Data.Context;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,8 +7,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
+
+builder.Services.AddDbContext<LothDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)

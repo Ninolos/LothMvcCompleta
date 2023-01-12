3a07d984da1e666f40d2746109cc24a1b62434e4
Jonathan Daniel Loth <77141599+Ninolos@users.noreply.github.com>
2023-01-12 08:33:43 -0300

    #dev - Mapping para o EF

---
+++ src/Loth.Data/Mappings/FornecedorMapping.cs
@@ -0,0 +1,40 @@
+﻿using AppLothMVC.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loth.Data.Mappings
+{
+    public class FornecedorMapping : IEntityTypeConfiguration<Fornecedor>
+    {
+        public void Configure(EntityTypeBuilder<Fornecedor> builder)
+        {
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.Nome)
+                .IsRequired()
+                .HasColumnType("varchar(200)");
+
+            builder.Property(p => p.Documento)
+                .IsRequired()
+                .HasColumnType("varchar(14)");
+
+            //config 1 : 1 => Fornecedor : Endereco
+
+            builder.HasOne(p => p.Endereco)
+                .WithOne(e => e.Fornecedor);
+
+            //config 1 : N => Fornecedor : Produtos, não esquecer a chave estrangeira
+
+            builder.HasMany(p => p.Produtos)
+                .WithOne(f => f.Fornecedor)
+                .HasForeignKey(f => f.FornecedorId);
+
+            builder.ToTable("Fornecedores");
+        }
+    }
+}
--- src/Loth.Data/Mappings/ProdutoMapping.cs
+++ src/Loth.Data/Mappings/ProdutoMapping.cs
@@ -14,6 +14,21 @@ namespace Loth.Data.Mappings
         public void Configure(EntityTypeBuilder<Produto> builder)
         {
             builder.HasKey(x => x.Id);
+
+            builder.Property(p => p.Nome)
+                .IsRequired()
+                .HasColumnType("varchar(200)");
+
+            builder.Property(p => p.Descricao)
+                .IsRequired()
+                .HasColumnType("varchar(1000)");
+
+            builder.Property(p => p.Imagem)
+                .IsRequired()
+                .HasColumnType("varchar(100)");
+
+            builder.ToTable("Produtos");
+
         }
     }
 }

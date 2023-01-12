246dd56c5e3aab5a80a9b7fdbff242af4e5d837a
Jonathan Daniel Loth <77141599+Ninolos@users.noreply.github.com>
2023-01-12 09:12:09 -0300

    #dev - configs Context

--- src/Loth.Data/Context/LothDbContext.cs
+++ src/Loth.Data/Context/LothDbContext.cs
@@ -17,5 +17,23 @@ namespace Loth.Data.Context
         public DbSet<Produto> Produtos { get; set; }
         public DbSet<Endereco> Enderecos { get; set; }
         public DbSet<Fornecedor> Fornecedores { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //evitar criação de strings nvarchar max
+
+            foreach (var property in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(e => e.GetProperties()
+                .Where(p => p.ClrType == typeof(string))))
+                property.SetColumnType("varchar(100)");
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(LothDbContext).Assembly);
+
+            //desativando o delete Cascade, evita que ao excluir um Fornecedor leve os produtos junto
+
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys())) relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }

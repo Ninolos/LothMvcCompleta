0d6e451ee9ad22a8b0b8dfe5511df4fc9cd60757
Jonathan Daniel Loth <77141599+Ninolos@users.noreply.github.com>
2023-01-13 08:51:57 -0300

    #dev - Criaçao das Interfaces de Repositório

---
+++ src/Loth.Business/Interfaces/IEnderecoRepository.cs
@@ -0,0 +1,14 @@
+﻿using AppLothMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loth.Business.Interfaces
+{
+    public interface IEnderecoRepository : IRepository<Endereco>
+    {
+        Task<Endereco> ObterEnderecoPorFornecedor(Guid fornecedorId);
+    }
+}
---
+++ src/Loth.Business/Interfaces/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using AppLothMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loth.Business.Interfaces
+{
+    public interface IRepository<TEntity> : IDisposable where TEntity : Entity
+    {
+        Task Adicionar(TEntity entity);
+        Task Atualizar(TEntity entity);
+        Task <TEntity> ObterPorId(Guid id);
+        Task <List<TEntity>> ObterTodos();
+        Task Remover(Guid id);
+
+        //Passa uma expressão Lambda que vai buscar oq vc quiser, se verdadeiro bool true
+        Task<IEnumerable<TEntity>> Buscar(Expression<Func<TEntity, bool>> expression);
+        Task<int> SaveChanges();
+    }
+}

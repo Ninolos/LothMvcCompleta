fefcc2cff6ca3c78fe08b4899e8c16499bf10c97
Jonathan Daniel Loth <77141599+Ninolos@users.noreply.github.com>
2023-02-21 13:49:23 -0300

    #dev -  inicio das regras com FluentValidation para Validar os campos na camada de Service

---
+++ src/Loth.Business/Services/FornecedorService.cs
@@ -0,0 +1,33 @@
+﻿using AppLothMVC.Models;
+using Loth.Business.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loth.Business.Services
+{
+    public class FornecedorService : BaseService, IFornecedorService
+    {
+        public Task Adicionar(Fornecedor fornecedor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Atualizar(Fornecedor fornecedor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AtualizarEndereco(Endereco endereco)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Remover(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Loth.Business/Services/ProdutoService.cs
@@ -0,0 +1,28 @@
+﻿using AppLothMVC.Models;
+using Loth.Business.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loth.Business.Services
+{
+    public class ProdutoService : BaseService, IProdutoService
+    {
+        public Task Adicionar(Produto produto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Atualizar(Produto produto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Remover(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

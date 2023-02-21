3c6cf6645c4a493e273279f845a1c57c1c389f78
Jonathan Daniel Loth <77141599+Ninolos@users.noreply.github.com>
2023-02-21 14:01:19 -0300

    #dev - BaseService para Notificar e metodo generico para Executar as Validacoes das Classes Validations

--- src/Loth.Business/Services/BaseService.cs
+++ src/Loth.Business/Services/BaseService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AppLothMVC.Models;
+using FluentValidation;
+using FluentValidation.Results;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,27 @@ namespace Loth.Business.Services
 {
     public abstract class BaseService
     {
+        protected void Notificar(ValidationResult validationResult)
+        {
+            foreach (var error in validationResult.Errors)
+            {
+                Notificar(error.ErrorMessage);
+            }
+        }
+
+        protected void Notificar(string mensagem)
+        {
+            
+        }
+        protected bool ExecutarValidacao<TV, TE>(TV validacao, TE entidade) where TV : AbstractValidator<TE> where TE : Entity
+        {
+            var validator = validacao.Validate(entidade);
+
+            if (validator.IsValid) return true;
+
+            Notificar(validator);
+
+            return false;
+        }
     }
 }

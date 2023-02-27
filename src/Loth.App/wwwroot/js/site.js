a0d887eabf117871b4e0349afaac36eb301cd194
Jonathan Daniel Loth <77141599+Ninolos@users.noreply.github.com>
2023-02-27 16:04:14 -0300

    #dev - correçoes

--- src/Loth.App/wwwroot/js/site.js
+++ src/Loth.App/wwwroot/js/site.js
@@ -1,127 +1,111 @@
 ﻿function SetModal() {
-    var modalLinks = document.querySelectorAll('a[data-modal]');
-    modalLinks.forEach(function (link) {
-        link.addEventListener('click', function (event) {
-            event.preventDefault(); // impede que o link execute sua ação padrão
 
-            var url = this.href; // obtém a URL do link
-            var xhr = new XMLHttpRequest(); // cria um objeto XMLHttpRequest
+    $(document).ready(function () {
+        $(function () {
+            $.ajaxSetup({ cache: false });
 
-            xhr.onload = function () {
-                if (xhr.status === 200) { // verifica se a requisição foi bem-sucedida
-                    var modalContent = document.getElementById('myModalContent');
-                    modalContent.innerHTML = xhr.responseText; // define o conteúdo do modal
-                    var modal = new bootstrap.Modal(document.getElementById('myModal')); // cria uma nova instância do modal do Bootstrap
-                    modal.show(); // exibe o modal
-                    bindForm(modalContent); // vincula o formulário ao modal
-                }
-            };
-
-            xhr.open('GET', url);
-            xhr.send();
+            $("a[data-modal]").on("click",
+                function (e) {
+                    $('#myModalContent').load(this.href,
+                        function () {
+                            $('#myModal').modal({
+                                keyboard: true
+                            },
+                                'show');
+                            bindForm(this);
+                        });
+                    return false;
+                });
         });
     });
 }
 
 function bindForm(dialog) {
-    var forms = dialog.getElementsByTagName('form');
-    for (var i = 0; i < forms.length; i++) {
-        forms[i].addEventListener('submit', function (event) {
-            event.preventDefault(); // impede que o formulário execute sua ação padrão
-
-            var xhr = new XMLHttpRequest(); // cria um objeto XMLHttpRequest
-
-            xhr.onload = function () {
-                if (xhr.status === 200) { // verifica se a requisição foi bem-sucedida
-                    var result = JSON.parse(xhr.responseText);
-                    if (result.success) {
-                        var modal = new bootstrap.Modal(document.getElementById('myModal'));
-                        modal.hide(); // esconde o modal
-                        var enderecoTarget = document.getElementById('EnderecoTarget');
-                        enderecoTarget.innerHTML = result.url; // define o conteúdo retornado na div EnderecoTarget
-                    } else {
-                        var modalContent = document.getElementById('myModalContent');
-                        modalContent.innerHTML = result;
-                        bindForm(dialog);
-                    }
+    $('form', dialog).submit(function () {
+        $.ajax({
+            url: this.action,
+            type: this.method,
+            data: $(this).serialize(),
+            success: function (result) {
+                if (result.success) {
+                    $('#myModal').modal('hide');
+                    $('#EnderecoTarget').load(result.url); // Carrega o resultado HTML para a div demarcada
+                } else {
+                    $('#myModalContent').html(result);
+                    bindForm(dialog);
                 }
-            };
-
-            xhr.open(this.method, this.action);
-            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-            xhr.send(new FormData(this));
-            SetModal();
-            return false;
-        });
-    }
-}
-
-function fecharModal() {
-    var modal = document.getElementById('myModal');
-    var backdrop = document.getElementsByClassName('modal-backdrop')[0];
-    modal.style.display = 'none';
-    backdrop.style.display = 'none';
-}
-
-function BuscaCep() {
-    var cepInput = document.getElementById('Endereco_Cep');
-    var logradouroInput = document.getElementById('Endereco_Logradouro');
-    var bairroInput = document.getElementById('Endereco_Bairro');
-    var cidadeInput = document.getElementById('Endereco_Cidade');
-    var estadoInput = document.getElementById('Endereco_Estado');
-
-    function limpa_formulário_cep() {
-        logradouroInput.value = '';
-        bairroInput.value = '';
-        cidadeInput.value = '';
-        estadoInput.value = '';
-    }
-
-    cepInput.addEventListener('blur', function () {
-        var cep = cepInput.value.replace(/\D/g, '');
-
-        if (cep !== '') {
-            var validacep = /^[0-9]{8}$/;
-
-            if (validacep.test(cep)) {
-                logradouroInput.value = '...';
-                bairroInput.value = '...';
-                cidadeInput.value = '...';
-                estadoInput.value = '...';
-
-                var xhr = new XMLHttpRequest();
-
-                xhr.onload = function () {
-                    if (xhr.status === 200) {
-                        var dados = JSON.parse(xhr.responseText);
-
-                        if (!dados.erro) {
-                            logradouroInput.value = dados.logradouro;
-                            bairroInput.value = dados.bairro;
-                            cidadeInput.value = dados.localidade;
-                            estadoInput.value = dados.uf;
-                        } else {
-                            limpa_formulário_cep();
-                            alert('CEP não encontrado.');
-                        }
-                    }
-                };
-
-                xhr.open('GET', 'https://viacep.com.br/ws/' + cep + '/json/');
-                xhr.send();
-            } else {
-                limpa_formulário_cep();
-                alert('Formato de CEP inválido.');
             }
-        } else {
-            limpa_formulário_cep();
-        }
+        });
+
+        SetModal();
+        return false;
     });
 }
 
-function SetMsgBox() {
-    var msgBox = document.getElementById('msg_box');
-    setTimeout(function () {
-        msgBox.style.display = 'none';
-    }, 2500);
+function BuscaCep() {
+    $(document).ready(function () {
+
+        function limpa_formulário_cep() {
+            // Limpa valores do formulário de cep.
+            $("#Endereco_Logradouro").val("");
+            $("#Endereco_Bairro").val("");
+            $("#Endereco_Cidade").val("");
+            $("#Endereco_Estado").val("");
+        }
+
+        //Quando o campo cep perde o foco.
+        $("#Endereco_Cep").blur(function () {
+
+            //Nova variável "cep" somente com dígitos.
+            var cep = $(this).val().replace(/\D/g, '');
+
+            //Verifica se campo cep possui valor informado.
+            if (cep != "") {
+
+                //Expressão regular para validar o CEP.
+                var validacep = /^[0-9]{8}$/;
+
+                //Valida o formato do CEP.
+                if (validacep.test(cep)) {
+
+                    //Preenche os campos com "..." enquanto consulta webservice.
+                    $("#Endereco_Logradouro").val("...");
+                    $("#Endereco_Bairro").val("...");
+                    $("#Endereco_Cidade").val("...");
+                    $("#Endereco_Estado").val("...");
+
+                    //Consulta o webservice viacep.com.br/
+                    $.getJSON("https://viacep.com.br/ws/" + cep + "/json/?callback=?",
+                        function (dados) {
+
+                            if (!("erro" in dados)) {
+                                //Atualiza os campos com os valores da consulta.
+                                $("#Endereco_Logradouro").val(dados.logradouro);
+                                $("#Endereco_Bairro").val(dados.bairro);
+                                $("#Endereco_Cidade").val(dados.localidade);
+                                $("#Endereco_Estado").val(dados.uf);
+                            } //end if.
+                            else {
+                                //CEP pesquisado não foi encontrado.
+                                limpa_formulário_cep();
+                                alert("CEP não encontrado.");
+                            }
+                        });
+                } //end if.
+                else {
+                    //cep é inválido.
+                    limpa_formulário_cep();
+                    alert("Formato de CEP inválido.");
+                }
+            } //end if.
+            else {
+                //cep sem valor, limpa formulário.
+                limpa_formulário_cep();
+            }
+        });
+    });
 }
+
+$(document).ready(function () {
+    $("#msg_box").fadeOut(2500);
+});
